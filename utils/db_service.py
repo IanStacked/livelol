a0297640e7b18687e91b597078abe6cebf6ac89e
IanStacked <ian.rodgers156@gmail.com>
2026-01-31 14:19:01 -0800

    Update db_service.py eror handling to properly use custom exceptions.

--- utils/db_service.py
+++ utils/db_service.py
@@ -2,7 +2,7 @@ from firebase_admin import firestore
 from google.cloud.firestore import FieldFilter
 
 from database import GUILD_CONFIG_COLLECTION, TRACKED_USERS_COLLECTION
-from utils.exceptions import DatabaseError
+from utils.exceptions import DatabaseError, UserNotFoundError
 from utils.logger_config import logger
 
 
@@ -83,8 +83,7 @@ class DatabaseService:
             doc_ref.set(payload, merge=True)
         except Exception as e:
             logger.exception(f"❌ ERROR: tracking: {e}")
-            await ctx.send("Database update failed")
-            raise DatabaseError(f"Database write failed for player {riot_id}") from e
+            raise DatabaseError(f"Database write failed for player {riot_id}.") from e
 
     async def untrack_user(self, ctx, riot_id, puuid):
         guild_id_str = str(ctx.guild.id)
@@ -93,11 +92,15 @@ class DatabaseService:
         try:
             doc = doc_ref.get()
             if not doc.exists:
-                return await ctx.send(f"{riot_id} is not in the database.")
+                raise UserNotFoundError(
+                    f"{riot_id} is not being tracked.",
+                )
             data = doc.to_dict()
             guild_list = data.get("guild_ids", [])
             if guild_id_str not in guild_list:
-                return await ctx.send(f"{riot_id} is not being tracked in this server.")
+                raise UserNotFoundError(
+                    f"{riot_id} is not being tracked in this server.",
+                )
             guild_list.remove(guild_id_str)
             if not guild_list:
                 # We are the only server left, delete the whole file
@@ -108,5 +111,4 @@ class DatabaseService:
                 doc_ref.set(data)
         except Exception as e:
             logger.exception(f"❌ ERROR: untracking: {e}")
-            await ctx.send("Database update failed")
-            raise DatabaseError(f"Database write failed for player {riot_id}") from e
+            raise DatabaseError(f"Database write failed for player {riot_id}.") from e
