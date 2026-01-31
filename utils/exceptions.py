53a67708495bff5864474d1f4df29c64b8e3d1ed
IanStacked <ian.rodgers156@gmail.com>
2026-01-31 14:09:39 -0800

    Add new default messages to all custom exceptions.

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,16 +1,36 @@
-class RiotAPIError(Exception):
-    """Base exception for all Riot API related errors."""
-    pass
+class LiveLOLError(Exception):
+    """Base exception for all bot errors."""
+    def __init__(self, message: str = "An unexpected error occurred."):
+        # We do not provide a "detail" variable, but rather go straight to message.
+        # This is because this is the ultimate base exception,
+        # there is no prefix information to add.
+        self.message = message
+        super().__init__(self.message)
 
+class RiotAPIError(LiveLOLError):
+    """Base exception for all Riot API related errors."""
+    def __init__(self, detail: str = "The Riot API was unresponsive."):
+        prefix = "📡 **Riot API Issue:**"
+        self.message = f"{prefix} {detail}"
+        super().__init__(self.message)
 
 class UserNotFoundError(RiotAPIError):
     """Raised when a users Riot ID is not recognized by Riot."""
-    pass
-
+    def __init__(self, detail: str = "Player not found."):
+        prefix = "🔍 **Search Error:**"
+        self.message = f"{prefix} {detail}"
+        super().__init__(self.message)
 
 class RateLimitError(RiotAPIError):
     """Raised when a rate limit was hit by an API call."""
-    pass
+    def __init__(self, detail: str = "API rate limit hit."):
+        prefix = ""
+        self.message = f"{prefix} {detail}"
+        super().__init__(self.message)
 
-class DatabaseError(Exception):
+class DatabaseError(LiveLOLError):
     """Base exception for all database related errors."""
+    def __init__(self, detail: str = "Failed to update database records."):
+        prefix = "💾 **Database Error:**"
+        self.message = f"{prefix} {detail}"
+        super().__init__(self.message)
