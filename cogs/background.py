4a0552e4c56310cc628232291cfe4ccda2dd0c44
IanStacked <ian.rodgers156@gmail.com>
2026-02-03 21:37:36 -0800

    Add tightly scoped error handling to prevent ServiceUnavailableError's from bubbling up and ending the background loop early.

--- cogs/background.py
+++ cogs/background.py
@@ -5,6 +5,7 @@ from discord.ext import commands, tasks
 from bot import RIOT_API_KEY
 from database import GUILD_CONFIG_COLLECTION, TRACKED_USERS_COLLECTION
 from utils.constants import REGION_CLUSTERS
+from utils.exceptions import ServiceUnavailableError
 from utils.helpers import check_new_riot_id, extract_match_info
 from utils.logger_config import logger
 from utils.riot_api import get_ranked_info, get_recent_match_info
@@ -37,12 +38,15 @@ class Background(commands.Cog):
                 old_lp = doc.get("LP")
                 puuid = doc.get("puuid")
                 region = doc.get("region")
-                data = await get_ranked_info(
-                    self.bot.session,
-                    puuid,
-                    region,
-                    RIOT_API_KEY,
-                )
+                try:
+                    data = await get_ranked_info(
+                        self.bot.session,
+                        puuid,
+                        region,
+                        RIOT_API_KEY,
+                    )
+                except ServiceUnavailableError:
+                    continue
                 new_tier = data.get("tier")
                 new_rank = data.get("rank")
                 new_lp = data.get("LP")
@@ -67,12 +71,15 @@ class Background(commands.Cog):
                     if channel:
                         riot_id = doc.get("riot_id")
                         cluster = REGION_CLUSTERS.get(region)
-                        match_info = await get_recent_match_info(
-                            self.bot.session,
-                            puuid,
-                            cluster,
-                            RIOT_API_KEY,
-                        )
+                        try:
+                            match_info = await get_recent_match_info(
+                                self.bot.session,
+                                puuid,
+                                cluster,
+                                RIOT_API_KEY,
+                            )
+                        except ServiceUnavailableError:
+                            continue
                         processed_match_info = extract_match_info(match_info, puuid)
                         ranked_data = {
                             "old_tier": old_tier,
