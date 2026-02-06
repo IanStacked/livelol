2bcf5b21fa5a44e98e171ef7389770c76a4e28ea
IanStacked <ian.rodgers156@gmail.com>
2026-02-05 18:53:40 -0800

    Refactor background process to use new helper functions, focused on improving maintainability.

--- cogs/background.py
+++ cogs/background.py
@@ -3,10 +3,15 @@ import asyncio
 from discord.ext import commands, tasks
 
 from bot import RIOT_API_KEY
-from database import GUILD_CONFIG_COLLECTION, TRACKED_USERS_COLLECTION
+from database import TRACKED_USERS_COLLECTION
 from utils.constants import REGION_CLUSTERS
 from utils.exceptions import ServiceUnavailableError
-from utils.helpers import check_new_riot_id, extract_match_info
+from utils.helpers import (
+    check_new_riot_id,
+    extract_match_info,
+    parse_rank_info,
+    rank_difference,
+)
 from utils.logger_config import logger
 from utils.riot_api import get_ranked_info, get_recent_match_info
 from utils.ui_components import MatchDetailsView
@@ -33,11 +38,11 @@ class Background(commands.Cog):
             docs = self.bot.db.collection(TRACKED_USERS_COLLECTION).stream()
             doc_list = list(docs)
             for doc in doc_list:
-                old_tier = doc.get("tier")
-                old_rank = doc.get("rank")
-                old_lp = doc.get("LP")
                 puuid = doc.get("puuid")
                 region = doc.get("region")
+                cluster = REGION_CLUSTERS.get(region)
+                riot_id = doc.get("riot_id")
+                guild_ids = doc.get("guild_ids")
                 try:
                     data = await get_ranked_info(
                         self.bot.session,
@@ -47,74 +52,43 @@ class Background(commands.Cog):
                     )
                 except ServiceUnavailableError:
                     continue
-                new_tier = data.get("tier")
-                new_rank = data.get("rank")
-                new_lp = data.get("LP")
-                if old_tier == new_tier and old_rank == new_rank and old_lp == new_lp:
+                ranked_data = parse_rank_info(doc, data)
+                if not rank_difference(ranked_data):
                     continue
                 doc.reference.update(data)
-                guild_ids = doc.get("guild_ids")
                 for guild in guild_ids:
-                    channel = None
+                    channel_id = self.bot.db_service.get_guild_config(guild)
+                    if channel_id is None:
+                        continue
+                    channel = self.bot.get_channel(channel_id)
                     try:
-                        config_ref = (
-                            self.bot.db.collection(GUILD_CONFIG_COLLECTION).document(guild)
-                        )
-                        config = config_ref.get()
-                        if config.exists:
-                            channel_id = config.get("channel_id")
-                            channel = self.bot.get_channel(channel_id)
-                    except Exception as e:
-                        logger.exception(
-                            f"❌ ERROR: fetching config for guild {guild}: {e}",
-                        )
-                    if channel:
-                        riot_id = doc.get("riot_id")
-                        cluster = REGION_CLUSTERS.get(region)
-                        try:
-                            match_info = await get_recent_match_info(
-                                self.bot.session,
-                                puuid,
-                                cluster,
-                                RIOT_API_KEY,
-                            )
-                        except ServiceUnavailableError:
-                            continue
-                        processed_match_info = extract_match_info(match_info, puuid)
-                        ranked_data = {
-                            "old_tier": old_tier,
-                            "old_rank": old_rank,
-                            "old_lp": old_lp,
-                            "new_tier": new_tier,
-                            "new_rank": new_rank,
-                            "new_lp": new_lp,
-                        }
-                        new_riot_id = check_new_riot_id(
-                            processed_match_info,
+                        match_info = await get_recent_match_info(
+                            self.bot.session,
                             puuid,
-                            riot_id,
+                            cluster,
+                            RIOT_API_KEY,
                         )
-                        if(new_riot_id != ""):
-                            #update database with new riot_id
-                            doc_id = puuid
-                            doc_ref = (
-                                self.bot.db.collection(TRACKED_USERS_COLLECTION).document(doc_id)
-                            )
-                            formatted_riot_id = {
-                                "riot_id": new_riot_id,
-                            }
-                            doc_ref.update(formatted_riot_id)
-                            riot_id = new_riot_id
-                        view = MatchDetailsView(
-                            processed_match_info,
-                            ranked_data,
-                            riot_id,
-                            puuid,
-                            region,
-                        )
-                        initial_embed = view.create_minimized_embed()
-                        message = await channel.send(embed=initial_embed, view=view)
-                        view.message = message
+                    except ServiceUnavailableError:
+                        continue
+                    processed_match_info = extract_match_info(match_info, puuid)
+                    new_riot_id = check_new_riot_id(
+                        processed_match_info,
+                        puuid,
+                        riot_id,
+                    )
+                    if(new_riot_id != ""):
+                        self.bot.db_service.update_riot_id(puuid, new_riot_id)
+                        riot_id = new_riot_id
+                    view = MatchDetailsView(
+                        processed_match_info,
+                        ranked_data,
+                        riot_id,
+                        puuid,
+                        region,
+                    )
+                    initial_embed = view.create_minimized_embed()
+                    message = await channel.send(embed=initial_embed, view=view)
+                    view.message = message
                 # This sleep ensures we stay behind API Rate limit curve.
                 await asyncio.sleep(1.5)
         except Exception as e:
